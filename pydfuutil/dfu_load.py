18c495f62fef26039fb22be870c508d9c24f44de
o-murphy <thehelixpg@gmail.com>
2023-06-01 21:34:33 +0000

    base definitions

---
+++ pydfuutil/dfu_load.py
@@ -0,0 +1,20 @@
+verbose: int = 0
+
+
+PROGRESS_BAR_WIDTH = 50
+
+
+def dfuload_do_upload(dif: 'dfu_if', xfer_size: int, file: 'dfu_file') -> int:
+	pass
+
+	
+def dfuload_do_dnload(dif: 'dfu_if', xfer_size: int, file: 'dfu_file') -> int:
+	pass
+	
+
+def dfuload_init() -> None:
+	pass
+	
+
+
+	
