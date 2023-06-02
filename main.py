26d4bbbe28c8378267932f8f36cebf3603ba196d
Murphy <thehelixpg@gmail.com>
2023-06-02 10:07:28 +0300

    dfu workwith ok

--- main.py
+++ main.py
@@ -1,194 +1,74 @@
-
+from pydfuutil.dfu import *
 import libusb_package
 import usb.backend.libusb1
 import usb.core
-# from pyfu_usb.descriptor import *
+from time import sleep
 
-
-DFU_DETACH = 0
-DFU_DNLOAD = 1
-DFU_UPLOAD = 2
-DFU_GETSTATUS = 3
-DFU_CLRSTATUS = 4
-DFU_GETSTATE = 5
-DFU_ABORT = 6
 
 dfu_timeout = 5000
 
 
-def dfu_get_status(device: usb.core.Device, interface):
-
-    buffer = bytes(6)
-
-    result = device.ctrl_transfer(
-        bmRequestType=usb.util.ENDPOINT_IN | usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_INTERFACE,
-        bRequest=DFU_GETSTATUS,
-        wValue=0,
-        wIndex=interface,
-        data_or_wLength=buffer,
-        # wLength=length,
-        timeout=dfu_timeout,
-    )
-    print('result', result)
-    buffer = result.tobytes()
-    # if result == 6:
-    print('bStatus', buffer[0])
-    print('bwPollTimeout', ((0xff & buffer[3]) << 16) | ((0xff & buffer[2]) << 8) | (0xff & buffer[1]))
-
-    print('bState', buffer[4])
-    print('iString', buffer[5])
-
-
-def dfu_upload(device: usb.core.Device, interface, length, transaction, data):
-    """
-    Upload data from the device using PyUSB library.
-
-    Args:
-        device: PyUSB device handle.
-        interface: USB device interface.
-        length: Length of the data to upload.
-        transaction: Transaction counter.
-        data: Data buffer to store the uploaded data.
-
-    Returns:
-        The status of the control transfer.
-    """
-    status = device.ctrl_transfer(
-        bmRequestType=usb.util.ENDPOINT_IN | usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_INTERFACE,
-        bRequest=DFU_UPLOAD,
-        wValue=transaction,
-        wIndex=interface,
-        data_or_wLength=data,
-        # wLength=length,
-        timeout=dfu_timeout,
-    )
-
-    return status
-
-
 USB_DT_DFU = 0x21
 
-# def found_dfu():
-"""
-struct dfu_if {
-    struct usb_dfu_func_descriptor func_dfu;
-    uint16_t quirks;
-    uint16_t busnum;
-    uint16_t devnum;
-    uint16_t vendor;
-    uint16_t product;
-    uint16_t bcdDevice;
-    uint8_t configuration;
-    uint8_t interface;
-    uint8_t altsetting;
-    uint8_t flags;
-    uint8_t bMaxPacketSize0;
-    char *alt_name;
-    char *serial_name;
-    libusb_device *dev;
-    libusb_device_handle *dev_handle;
-    struct dfu_if *next;
-};"""
+
+def get_dev_h():
+    libusb1_backend = usb.backend.libusb1.get_backend(find_library=libusb_package.find_library)
+    devh = usb.core.find(backend=libusb1_backend, idVendor=0x1FC9, idProduct=0x000C)
+    return devh
 
 
-libusb1_backend = usb.backend.libusb1.get_backend(find_library=libusb_package.find_library)
-dev = usb.core.find(backend=libusb1_backend, idVendor=0x1FC9, idProduct=0x000C)
-desc = usb.util.find_descriptor(dev)
+def get_dev_descriptor(devh):
+    desc = usb.util.find_descriptor(devh)
+    return desc
 
-# print(dev)
-# print(desc)
-# print(desc.interfaces()[0].__dir__())
-# print(desc.interfaces()[0].alternate_index)
-# print(desc.interfaces()[0].index)
-# print(desc.interfaces()[0].configuration)
-print([hex(i) for i in desc.interfaces()[0].extra_descriptors])
 
-# dev.reset()
+def get_intf_descriptor(devh: usb.core.Device, desc) -> usb.core.Interface:
+    print(f'Inrf found: {len(desc.interfaces())}')
+    return desc.interfaces()[0]
 
-ext = usb.util.find_descriptor(
-                desc.interfaces()[0].extra_descriptors,
-                custom_match=lambda d: d == USB_DT_DFU
-            )
 
-print('ext', ext)
+def get_intf_extra(intf):
+    print(f'Extra found: {len(intf.extra_descriptors)}')
+
+    ext = usb.util.find_descriptor(
+        intf.extra_descriptors,
+        custom_match=lambda d: d == USB_DT_DFU
+    )
+    return ext
 
 
 
+if __name__ == '__main__':
+    offset = 532480
+    start = int((offset + 4096) / 2048)
+    data = bytes(2048)
 
-offset = 532480
-start = int((offset + 4096) / 2048)
-print(start)
+    # intf: usb.core.Interface = usb.util.find_descriptor(
+    #                 dev[0],
+    #                 custom_match=lambda d: d.bDescriptorType == 0x4
+    #             )
+    # print(intf)
 
-intf: usb.core.Interface = usb.util.find_descriptor(
-                dev[0],
-                custom_match=lambda d: d.bDescriptorType == 0x4
-            )
-print(intf)
+    devh = get_dev_h()
 
+    desc = get_dev_descriptor(devh)
+    intf = get_intf_descriptor(devh, desc)
 
-"""
-struct usb_dfu_func_descriptor {
-	uint8_t		bLength;
-	uint8_t		bDescriptorType;
-	uint8_t		bmAttributes;
-#define USB_DFU_CAN_DOWNLOAD	(1 << 0)
-#define USB_DFU_CAN_UPLOAD	(1 << 1)
-#define USB_DFU_MANIFEST_TOL	(1 << 2)
-#define USB_DFU_WILL_DETACH	(1 << 3)
-	uint16_t		wDetachTimeOut;
-	uint16_t		wTransferSize;
-	uint16_t		bcdDFUVersion;
-#ifdef _MSC_VER
-};
-"""
+    dfu_get_status(devh, intf.bInterfaceNumber)
 
-# Retrieve additional information about the device and interface
-if intf.bLength == 7:
-    print("Deducing device DFU version from functional descriptor length")
-    intf.bcdDFUVersion = 0x0100
-elif intf.bLength < 9:
-    print("Error obtaining DFU functional descriptor")
-    print("Please report this as a bug!")
-    print("Warning: Assuming DFU version 1.0")
-    intf.bcdDFUVersion = 0x0100
-    print("Warning: Transfer size can not be detected")
-    intf.wTransferSize = 0
+    usb.util.claim_interface(devh, intf.bInterfaceNumber)
+    dfu_detach(devh, intf.bInterfaceNumber, 1000)
+    usb.util.release_interface(devh, intf.bInterfaceNumber)
 
-for intf in desc.interfaces():
-    if intf.bInterfaceClass != 0xfe or intf.bInterfaceSubClass != 1:
-        continue
-
-    dfu_mode = intf.bInterfaceProtocol == 2
-    # print('bcdDFUVersion', intf.bcdDFUVersion)
-
-# intf.bcdDFUVersion = 0x011a
-# intf.bInterfaceProtocol = 0
+    dfu_get_status(devh, intf.bInterfaceNumber)
 
 
-dfu_get_status(dev, intf.bInterfaceNumber)
-
-setalt = intf.set_altsetting()
-print('\tsetalt', setalt)
-
-# ret  = usb.util.release_interface(dev, intf)
-# print('release_interface', ret)
-
-dev.set_interface_altsetting(intf.bInterfaceNumber, 0)
-
-dev.reset()
-
-dfu_get_status(dev, intf.bInterfaceNumber)
-
-from time import sleep
-
-sleep(0.5)
-
-data = bytes(2048)
+    # dfu_clear_status(devh, intf.bInterfaceNumber)
 
 
-print(intf.bInterfaceNumber)
+    dfu_get_status(devh, intf.bInterfaceNumber)
 
-# a = dfu_upload(dev, intf.iInterface, 2048, start, 2048 + offset)
-# a = dfu_upload(dev, intf.bInterfaceNumber, 2048, start, data)
-a = dfu_upload(dev, intf.bInterfaceNumber, 2048, start, data)
-print(a.tobytes())
+    a = dfu_upload(devh, intf.bInterfaceNumber, start, data)
+    # print(a.tobytes())
+
+    # setalt = intf.set_altsetting()
--- pydfuutil/dfu.py
+++ pydfuutil/dfu.py
@@ -1,141 +1,201 @@
 from enum import IntEnum
+
+import usb.util
+import usb.core
 from construct import *
 
 
 # dfu.h
 class DFUStates(IntEnum):
-	STATE_APP_IDLE = 0x00
-	STATE_APP_DETACH = 0x01
-	STATE_DFU_IDLE = 0x02
-	STATE_DFU_DOWNLOAD_SYNC = 0x03
-	STATE_DFU_DOWNLOAD_BUSY = 0x04
-	STATE_DFU_DOWNLOAD_IDLE = 0x05
-	STATE_DFU_MANIFEST_SYNC = 0x06
-	STATE_DFU_MANIFEST = 0x07
-	STATE_DFU_MANIFEST_WAIT_RESET = 0x08
-	STATE_DFU_UPLOAD_IDLE = 0x09
-	STATE_DFU_ERROR = 0x0a
-	
+    STATE_APP_IDLE = 0x00
+    STATE_APP_DETACH = 0x01
+    STATE_DFU_IDLE = 0x02
+    STATE_DFU_DOWNLOAD_SYNC = 0x03
+    STATE_DFU_DOWNLOAD_BUSY = 0x04
+    STATE_DFU_DOWNLOAD_IDLE = 0x05
+    STATE_DFU_MANIFEST_SYNC = 0x06
+    STATE_DFU_MANIFEST = 0x07
+    STATE_DFU_MANIFEST_WAIT_RESET = 0x08
+    STATE_DFU_UPLOAD_IDLE = 0x09
+    STATE_DFU_ERROR = 0x0a
+
 
 class DFUStatus(IntEnum):
-	DFU_STATUS_OK = 0x00
-	DFU_STATUS_ERROR_TARGET = 0x01
-	DFU_STATUS_ERROR_FILE = 0x02
-	DFU_STATUS_ERROR_WRITE = 0x03
-	DFU_STATUS_ERROR_ERASE = 0x04
-	DFU_STATUS_ERROR_CHECK_ERASED = 0x05
-	DFU_STATUS_ERROR_PROG = 0x06
-	DFU_STATUS_ERROR_VERIFY = 0x07
-	DFU_STATUS_ERROR_ADDRESS = 0x08
-	DFU_STATUS_ERROR_NOTDONE = 0x09
-	DFU_STATUS_ERROR_FIRMWARE = 0x0a
-	DFU_STATUS_ERROR_VENDOR = 0x0b
-	DFU_STATUS_ERROR_USBR = 0x0c
-	DFU_STATUS_ERROR_POR = 0x0d
-	DFU_STATUS_ERROR_UNKNOWN = 0x0e
-	DFU_STATUS_ERROR_STALLEDPKT = 0x0f
+    DFU_STATUS_OK = 0x00
+    DFU_STATUS_ERROR_TARGET = 0x01
+    DFU_STATUS_ERROR_FILE = 0x02
+    DFU_STATUS_ERROR_WRITE = 0x03
+    DFU_STATUS_ERROR_ERASE = 0x04
+    DFU_STATUS_ERROR_CHECK_ERASED = 0x05
+    DFU_STATUS_ERROR_PROG = 0x06
+    DFU_STATUS_ERROR_VERIFY = 0x07
+    DFU_STATUS_ERROR_ADDRESS = 0x08
+    DFU_STATUS_ERROR_NOTDONE = 0x09
+    DFU_STATUS_ERROR_FIRMWARE = 0x0a
+    DFU_STATUS_ERROR_VENDOR = 0x0b
+    DFU_STATUS_ERROR_USBR = 0x0c
+    DFU_STATUS_ERROR_POR = 0x0d
+    DFU_STATUS_ERROR_UNKNOWN = 0x0e
+    DFU_STATUS_ERROR_STALLEDPKT = 0x0f
 
 
 class DFUCommands(IntEnum):
-	DFU_DETACH = 0
-	DFU_DNLOAD = 1
-	DFU_UPLOAD = 2
-	DFU_GETSTATUS = 3
-	DFU_CLRSTATUS = 4
-	DFU_GETSTATE = 5
-	DFU_ABORT = 6
+    DFU_DETACH = 0
+    DFU_DNLOAD = 1
+    DFU_UPLOAD = 2
+    DFU_GETSTATUS = 3
+    DFU_CLRSTATUS = 4
+    DFU_GETSTATE = 5
+    DFU_ABORT = 6
 
 
-#/* DFU interface */
+# /* DFU interface */
 DFU_IFF_DFU = 0x0001
 
 
-#/* DFU Mode, (not Runtime) */
+# /* DFU Mode, (not Runtime) */
 class DFUMode(IntEnum):
-	DFU_IFF_VENDOR = 0x0100
-	DFU_IFF_PRODUCT = 0x0200
-	DFU_IFF_CONFIG = 0x0400
-	DFU_IFF_IFACE = 0x0800
-	DFU_IFF_ALT = 0x1000
-	DFU_IFF_DEVNUM = 0x2000
-	DFU_IFF_PATH = 0x4000
-	
+    DFU_IFF_VENDOR = 0x0100
+    DFU_IFF_PRODUCT = 0x0200
+    DFU_IFF_CONFIG = 0x0400
+    DFU_IFF_IFACE = 0x0800
+    DFU_IFF_ALT = 0x1000
+    DFU_IFF_DEVNUM = 0x2000
+    DFU_IFF_PATH = 0x4000
+
 
 dfu_status = Struct(
-	'bStatus' / Byte,
-	'bwPollTimeout' / Int16ul,
-	'bState' / Byte,
-	'iString' / Byte
+    'bStatus' / Byte,
+    'bwPollTimeout' / Int16ul,
+    'bState' / Byte,
+    'iString' / Byte
 )
 
-
-#Todo: dataclass typed dictor Struct
+# Todo: dataclass typed dictor Struct
 dfu_if = Struct(
-	'vendor' / Int16ul,
-	'product' / Int16ul,
-	'bcdDevice' / Int16ul,
-	'configuration' / Int8ul,
-	'interface' / Int8ul,
-	'altsetting' / Int8ul,
-	'alt_name' / CString,
-	'bus' / Int16ul,
-	'devnum' / Int8ul,
-	'path' / CString,
-	'flags' / Int16ul,
-	'count' / Int16ul,
-	#'dev' / libusb_device,
-	#'dev_handle' / libusb_device_handle,
+    'vendor' / Int16ul,
+    'product' / Int16ul,
+    'bcdDevice' / Int16ul,
+    'configuration' / Int8ul,
+    'interface' / Int8ul,
+    'altsetting' / Int8ul,
+    'alt_name' / CString('utf-8'),
+    'bus' / Int16ul,
+    'devnum' / Int8ul,
+    'path' / CString('utf-8'),
+    'flags' / Int16ul,
+    'count' / Int16ul,
+    # 'dev' / libusb_device,
+    # 'dev_handle' / libusb_device_handle,
 )
 
 
 def dfu_init(timeout: int) -> None:
-	pass
+    pass
 
-	
+
 def dfu_debug(level: int) -> None:
-	pass
+    pass
 
-	
-def dfu_detach(device: 'Device', interface: int, timeout: int) -> int:
-	pass
+
+def dfu_detach(device: usb.core.Device, interface: int, timeout: int) -> int:
+    return device.ctrl_transfer(
+        bmRequestType=usb.util.ENDPOINT_IN | usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_INTERFACE,
+        bRequest=DFUCommands.DFU_DETACH,
+        wValue=timeout,
+        wIndex=interface,
+        data_or_wLength=None,
+        # wLength=length,
+        timeout=dfu_timeout,
+    )
 
 
 def dfu_download(device: 'Device', interface: int, length: int, data: bytes) -> int:
-	pass
-
-	
-def dfu_upload(device: 'Device', interface: int, length: int, data: bytes) -> int:
-	pass
-
-	
-def dfu_get_status(device: 'Device', interface: int, status: dfu_status) -> int:
-	pass
+    pass
 
 
-def dfu_clear_status(device: 'Device', interface: int) -> int:
-	pass
+def dfu_upload(device: usb.core.Device, interface: int, transaction: int, data: bytes) -> int:
+    """
+    Upload data from the device using PyUSB library.
 
-	
+    Args:
+        device: PyUSB device handle.
+        interface: USB device interface.
+        length: Length of the data to upload.
+        transaction: Transaction counter.
+        data: Data buffer to store the uploaded data.
+
+    Returns:
+        The status of the control transfer.
+    """
+
+    return device.ctrl_transfer(
+        bmRequestType=usb.util.ENDPOINT_IN | usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_INTERFACE,
+        bRequest=DFUCommands.DFU_UPLOAD,
+        wValue=transaction,
+        wIndex=interface,
+        data_or_wLength=data,
+        # wLength=length,
+        timeout=dfu_timeout,
+    )
+
+
+# TODO: int out
+def dfu_get_status(device: usb.core.Device, interface: int) -> int:
+    buffer = bytes(6)
+
+    status = device.ctrl_transfer(
+        bmRequestType=usb.util.ENDPOINT_IN | usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_INTERFACE,
+        bRequest=DFUCommands.DFU_GETSTATUS,
+        wValue=0,
+        wIndex=interface,
+        data_or_wLength=buffer,
+        # wLength=length,
+        timeout=dfu_timeout,
+    )
+    # print('result', status)
+    buffer = status.tobytes()
+    # print('bStatus', buffer[0], DFUStatus(buffer[0]).name)
+    # print('bwPollTimeout', ((0xff & buffer[3]) << 16) | ((0xff & buffer[2]) << 8) | (0xff & buffer[1]))
+    #
+    # print('bState', buffer[4], DFUStates(buffer[4]).name)
+    # print('iString', buffer[5])
+    print(dfu_status.parse(buffer), DFUStatus(buffer[0]).name, DFUStates(buffer[4]).name)
+    return dfu_status.parse(buffer)
+
+
+def dfu_clear_status(device: usb.core.Device, interface: int) -> int:
+    status = device.ctrl_transfer(
+        bmRequestType=usb.util.ENDPOINT_IN | usb.util.CTRL_TYPE_CLASS | usb.util.CTRL_RECIPIENT_INTERFACE,
+        bRequest=DFUCommands.DFU_CLRSTATUS,
+        wValue=0,
+        wIndex=interface,
+        data_or_wLength=None,
+        # wLength=length,
+        timeout=dfu_timeout,
+    )
+    return status.tobytes()
+
+
 def dfu_get_state(device: 'Device', interface: int) -> int:
-	pass
+    pass
 
-	
+
 def dfu_abort(device: 'Device', interface: int) -> int:
-	pass
+    pass
 
-	
+
 def dfu_state_to_string(state: int) -> str:
-	pass
+    pass
 
-	
+
 def dfu_status_to_string(status: int) -> str:
-	pass
+    pass
 
-	
+
 debug: int = 0
 
-
-#dfu.c
+# dfu.c
 
 INVALID_DFU_TIMEOUT = -1
 
@@ -143,4 +203,3 @@ dfu_timeout: int = INVALID_DFU_TIMEOUT
 transaction: int = 0
 
 dfu_debug_level: int = 0
-
