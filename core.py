6bc3cc3ea0410193a785e33759d5a9b0be0cc6a3
Murphy <thehelixpg@gmail.com>
2023-06-08 13:11:26 +0300

    dfu core updated

--- core.py
+++ core.py
@@ -6,8 +6,10 @@ import construct
 
 import libusb_package
 import usb.core
+from rich import progress
 from usb.backend import libusb1
 
+from progress import DfuProgress
 from pydfuutil import dfu
 from pydfuutil.usb_dfu import USB_DFU_FUNC_DESCRIPTOR
 
@@ -15,6 +17,15 @@ from pydfuutil.usb_dfu import USB_DFU_FUNC_DESCRIPTOR
 # setting global params
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+_progress_bar = DfuProgress(
+    progress.TextColumn("[progress.description]{task.description}"),
+    progress.BarColumn(20),
+    progress.TaskProgressColumn(),
+    progress.TimeRemainingColumn(),
+    progress.DownloadColumn(),
+    progress.TransferSpeedColumn(),
+)
 
 construct.lib.setGlobalPrintFalseFlags(True)
 
@@ -57,6 +68,12 @@ class DfuDevice(usb.core.Device):
 
         dfu.dfu_init(dfu_timeout if dfu_timeout else 5000)
 
+    @property
+    def dfu_intf(self) -> [int, None]:
+        if self.dfu_interface is not None:
+            return self.dfu_interface.bInterfaceNumber
+        return None
+
     def get_dfu_descriptor(self, interface: usb.core.Interface):
         try:
             extra = interface.extra_descriptors
@@ -83,7 +100,7 @@ class DfuDevice(usb.core.Device):
             logger.error(f'No DFU interface found: {self._str()}')
 
     def get_status(self) -> (int, dict):
-        _, status = dfu.dfu_get_status(self, self.dfu_interface.bInterfaceNumber)
+        _, status = dfu.dfu_get_status(self, self.dfu_intf)
         sleep(status.bwPollTimeout)
         sleep(0.5)
         return _, status
@@ -95,18 +112,18 @@ class DfuDevice(usb.core.Device):
             if status.bState in [dfu.DFUState.APP_IDLE, dfu.DFUState.APP_DETACH]:
                 return False
             elif status.bState == dfu.DFUState.DFU_ERROR:
-                if dfu.dfu_clear_status(self, self.dfu_interface.bInterfaceNumber) < 0:
+                if dfu.dfu_clear_status(self, self.dfu_intf) < 0:
                     return False
                 _, status = self.get_status()
             elif status.bState in [dfu.DFUState.DFU_DOWNLOAD_IDLE, dfu.DFUState.DFU_UPLOAD_IDLE]:
-                if dfu.dfu_abort(self, self.dfu_interface.bInterfaceNumber) < 0:
+                if dfu.dfu_abort(self, self.dfu_intf) < 0:
                     return False
                 _, status = self.get_status()
             else:
                 break
 
         if status.bStatus != dfu.DFUStatus.OK:
-            if dfu.dfu_clear_status(self, self.dfu_interface.bInterfaceNumber) < 0:
+            if dfu.dfu_clear_status(self, self.dfu_intf) < 0:
                 return False
             _, status = self.get_status()
             if _ < 0:
@@ -117,9 +134,6 @@ class DfuDevice(usb.core.Device):
 
         return True
 
-    def probe(self):
-        ...
-
     def connect(self):
         if self.num_connect_attempts > 0:
             self.num_connect_attempts -= 1
@@ -128,15 +142,11 @@ class DfuDevice(usb.core.Device):
                 raise IOError(f'No DFU interface found: {self._str()}')
 
             if not self.is_connect_valid():
-                dfu.dfu_detach(self, self.dfu_interface.bInterfaceNumber, 1000)
+                dfu.dfu_detach(self, self.dfu_intf, 1000)
                 sleep(1)
                 self.reconnect()
         else:
             raise ConnectionError(f"Can't connect device: {self._str()}")
-
-    def disconnect(self):
-        usb.util.release_interface(self, self.dfu_interface.bInterfaceNumber)
-        usb.util.dispose_resources(self)
 
     def reconnect(self):
         
@@ -156,8 +166,53 @@ class DfuDevice(usb.core.Device):
         self.__dict__.update(dev_handle.__dict__)
         self.connect()
 
-    def do_upload(self):
-        ...
+    def disconnect(self):
+        usb.util.release_interface(self, self.dfu_intf)
+        usb.util.dispose_resources(self)
+
+    def do_upload(self, offset: int, length: int, page_size: int = 2048, callback=None):
+        USB_PAGE = page_size
+
+        total: int = length
+        start_page: int = int(offset / USB_PAGE)
+        page = start_page
+        ret = bytes()
+
+        upload_task = _progress_bar.add_task(
+            '[magenta1]Starting upload',
+            total=total
+        )
+
+        _progress_bar.callback = callback
+        _progress_bar.start()
+
+        try:
+
+            while True:
+
+                rc = dfu.dfu_upload(self, self.dfu_intf, page, USB_PAGE),
+
+                if len(rc[0]) < 0:
+                    ret = rc
+                    break
+
+                _progress_bar.update(upload_task, advance=USB_PAGE, description='[magenta1]Uploading...')
+
+                ret += rc[0]
+                if len(rc[0]) < USB_PAGE or (len(ret) >= total >= 0):
+                    break
+                page += 1
+
+        except usb.core.USBTimeoutError:
+            pass
+
+        dfu.dfu_upload(self, self.dfu_intf, page, 0),
+
+        _progress_bar.update(upload_task, advance=0, description='[yellow4]Upload finished!')
+        _progress_bar.stop()
+        _progress_bar.remove_task(upload_task)
+
+        return ret
 
     def do_download(self):
         ...
@@ -167,9 +222,14 @@ dfudev: DfuDevice = find(idVendor=0x1FC9, idProduct=0x000C)
 
 if dfudev is not None:
     dfudev.connect()
-    # usb.util.release_interface(dfudev, dfudev.dfu_interface.bInterfaceNumber)
+
+    print(dfudev)
+
+    # ret = dfudev.do_upload(0, -1, 2048)
+
     dfudev.disconnect()
 
-    while True:
-        sleep(10)
-    # dfudev.reset()
+    # with open('dump.bin', 'wb') as fp:
+    #     fp.write(ret)
+    # print(len(ret))
+
