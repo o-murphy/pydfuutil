66f0d2cc3ad6e182afbed605a529ac314e2378be
o-murphy <thehelixpg@gmail.com>
2024-05-09 01:55:17 +0300

    trying to renew with dfu_util to update it to v0.11

--- pydfuutil/_usb_util.py
+++ pydfuutil/_usb_util.py
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Generator
 
 from pydfuutil.dfu import DfuIf, Mode
