ed2c8e5b7105c058576e3df50b97ecd9e8ac7457
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2024-05-09 19:50:58 +0300

    dfu_func_desc probe ok

--- pydfuutil/_usb_util.py
+++ pydfuutil/_usb_util.py
@@ -16,14 +16,17 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
-
+import struct
+import sys
 from dataclasses import dataclass
 from typing import Generator
 
-from pydfuutil.dfu import DfuIf, Mode
 import usb.core
 
+from pydfuutil.dfu import DfuIf, Mode
 from pydfuutil.logger import logger
+from pydfuutil.quirks import get_quirks
+from pydfuutil.usb_dfu import USB_DT_DFU, USB_DT_DFU_SIZE, FuncDescriptor, BmAttributes
 
 _logger = logger.getChild(__name__.rsplit('.', maxsplit=1)[-1])
 
@@ -33,27 +36,64 @@ MAX_PATH_LEN = 20
 
 @dataclass
 class DfuUtil:
-    dfu_if: DfuIf
-    match_path: str
-    match_vendor: int
-    match_product: int
-    match_vendor_dfu: int
-    match_product_dfu: int
-    match_config_index: int
-    match_iface_index: int
-    match_iface_alt_index: int
-    match_devnum: int
-    match_iface_alt_name: str
-    match_serial: str
-    match_serial_dfu: str
+    dfu_if: DfuIf = None
+    match_path: str = None
+    match_vendor: int = -1
+    match_product: int = -1
+    match_vendor_dfu: int = -1
+    match_product_dfu: int = -1
+    match_config_index: int = -1
+    match_iface_index: int = -1
+    match_iface_alt_index: int = -1
+    match_devnum: int = -1
+    match_iface_alt_name: str = None
+    match_serial: str = None
+    match_serial_dfu: str = None
 
     dfu_root: DfuIf = None
 
     path_buf: str = None
 
 
-def find_descriptor():
-    raise NotImplementedError
+def cpu_to_le16(value: int) -> bytes:
+    """Convert int to uint16le"""
+    return value.to_bytes(2, byteorder='little')
+
+
+def le16_to_cpu(data: bytes) -> int:
+    """Convert uint16le to int"""
+    return int.from_bytes(data, byteorder='little')
+
+
+def find_descriptor(desc_list: list[int], desc_type: int, res_buf: bytearray):
+    """
+    Look for a descriptor in a concatenated descriptor list. Will
+    return upon the first match of the given descriptor type. Returns length of
+    found descriptor, limited to res_size
+    :return: 
+    """
+    p = 0
+    res_size = len(res_buf)
+    list_len = len(desc_list)
+
+    if list_len < 2:
+        return -1
+
+    while p + 1 < list_len:
+        desclen = desc_list[p]
+        if desclen == 0:
+            print("Invalid descriptor list")
+            return -1
+        if desc_list[p + 1] == desc_type:
+            if desclen > res_size:
+                desclen = res_size
+            if p + desclen > list_len:
+                desclen = list_len - p
+            res_buf[:] = desc_list[p:p + desclen]
+            return 0
+        p += desc_list[p]
+
+    return -1
 
 
 def get_utf8_string_descriptor():
@@ -151,3 +191,115 @@ __all__ = (
     'print_dfu_if',
     'list_dfu_interfaces',
 )
+
+
+
+
+def probe_dfu_func_descriptor():
+    func_dfu = FuncDescriptor()
+
+    def find_func_desc(dev: usb.core.Device,
+                       func_dfu: FuncDescriptor) -> int:
+        try:
+            ret_val = usb.control.get_descriptor(
+                dev, usb.DT_CONFIG_SIZE, usb.DT_CONFIG, 0
+            )
+            data = ret_val.tobytes()
+            (func_dfu.bLength,
+             func_dfu.bDescriptorType,
+             bmAttributes,
+             func_dfu.wDetachTimeOut,
+             func_dfu.wTransferSize,
+             func_dfu.bcdDFUVersion) = struct.unpack(
+                '<BBBHHH', data)
+
+            func_dfu.bmAttributes = BmAttributes(bmAttributes)
+            return 0
+        except usb.control.USBError:
+            return -1
+
+
+    def found_dfu() -> None:
+        print('found dfu')
+        if func_dfu.bLength == 7:
+            _logger.info("Deducing device DFU version from functional descriptor length")
+        elif func_dfu.bLength < 9:
+            _logger.error("Error obtaining DFU functional descriptor")
+            _logger.error("Please report this as a bug!")
+            _logger.warning("Assuming DFU version 1.0")
+            func_dfu.bcdDFUVersion = 0x0100
+            _logger.warning("Transfer size can not be detected")
+            func_dfu.wTransferSize = 0
+
+        intf: usb.core.Interface
+        for intf in dev.get_active_configuration():
+
+            if (DfuUtil.match_iface_index > -1
+                    and DfuUtil.match_iface_index != intf.index):
+                continue
+
+            if not intf:
+                break
+
+            num_altsettings = 0
+            for alt in intf:
+                num_altsettings += 1
+
+            multiple_alt = num_altsettings > 0
+
+        # for alt_idx in range(intf.bNumEndpoints):
+        #     quirks = get_quirks(dev.idVendor, dev.idProduct, dev.bcdDevice)
+        #
+        #     intf = uif.endpoints()
+        #     print('ends', intf)
+
+    dev: usb.core.Device = usb.core.find(find_all=False, idVendor=0x1fc9)
+    cfgs = dev.configurations()
+    for cfg in cfgs:
+
+        # NOTE: no need to find on cfg
+        ret = find_func_desc(dev, func_dfu)
+        if ret > -1:
+            # goto found_dfu
+            return found_dfu()
+
+        uif: usb.core.Interface
+        for uif in cfg:
+            if not uif:
+                break
+
+            if uif.bInterfaceClass != 0xfe or uif.bInterfaceSubClass != 1:
+                continue
+
+            # NOTE: no need to find on intf
+            ret = find_func_desc(dev, func_dfu)
+            if ret > -1:
+                # goto found_dfu
+                return found_dfu()
+
+            DfuUtil.has_dfu = 1
+
+        if DfuUtil.has_dfu == 1:
+            # Finally try to retrieve it requesting the
+            # device directly This is not supported on
+            # all devices for non-standard types
+
+            # NOTE: no need to find on intf
+            ret = find_func_desc(dev, func_dfu)
+            if ret > -1:
+                # goto found_dfu
+                return found_dfu()
+
+            _logger.warning("Device has DFU interface, "
+                            "but has no DFU functional descriptor")
+
+            # fake version 1.0
+            func_dfu.bLength = 7
+            func_dfu.bcdDFUVersion = 0x0100
+
+            # goto found_dfu
+            return found_dfu()
+
+
+if __name__ == '__main__':
+    probe_dfu_func_descriptor()
